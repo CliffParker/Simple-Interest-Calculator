6ce9a3b0bd1037b3ad196808d6cb95722592dc43
Clifford Allotey <cliffparker162@gmail.com>
2021-06-14 08:44:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function Output_funtion () {
+
+    var P = new Number(document.getElementById("Amount").value);
+
+    var IR = new Number(document.getElementById("Interest_Rate").value);
+
+    var T = new Number(document.getElementById("Years").value);
+
+    var Am = P * (IR*T/100);
+
+    var D = 1*(2020+T);
+    
+    document.getElementById("Output").innerHTML = 'If you deposit ' + P + ',' +
+     '<br> at an interest rate of ' + IR + '%.' +
+       '<br> You will  receive an amount of ' + Am + ',' +
+        '<br> in the year ' + D    ;
+
+}
+
+
